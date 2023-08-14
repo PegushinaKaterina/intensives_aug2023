b2db8a32221b299cc150e1e27a6f39d7c9f0e250
katya <katusha.pegushina@yandex.ru>
2023-08-14 19:14:46 +0300

    insertion sort

--- src/main/java/ru/yandex/tasks/Task3InsertionSort.java
+++ src/main/java/ru/yandex/tasks/Task3InsertionSort.java
@@ -10,7 +10,27 @@ public class Task3InsertionSort {
          * Выход: отсортированный (сортировкой вставками!) numbers
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+
+        ArrayList<Integer> sortedNumbers = new ArrayList<>();
+
+        for (Integer number : numbers) {
+            boolean wasAdded = false;
+            int a = number;
+            int j = 0;
+            while (!wasAdded && j < sortedNumbers.size()) {
+                if (a < sortedNumbers.get(j)) {
+                    sortedNumbers.add(j, a);
+                    wasAdded = true;
+                }
+                j++;
+            }
+
+            if (!wasAdded) {
+                sortedNumbers.add(a);
+            }
+
+        }
+        return sortedNumbers;
     }
 
     public static void selfCheck() {
